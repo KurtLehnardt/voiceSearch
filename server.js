c8926639f220eaf10bd0a465b7ce5c4bac5f7db3
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2020-07-12 14:52:31 -0600

    update port for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000 // || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname})
